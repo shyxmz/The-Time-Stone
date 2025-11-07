dfdaa803e43b42703192fb94dcba6b373e093cb8
shyxmz <shyammodi1729@gmail.com>
2025-11-08 00:44:07 +0530

    Extract constants (best practice they say)

---
+++ constants.js
@@ -0,0 +1,11 @@
+const STONE_COLORS = {
+    TIME: '#00ff88',
+    SPACE: 'blue',
+    POWER: 'purple',
+    MIND: 'yellow', 
+    REALITY: 'red',
+    SOUL: 'orange'
+};
+
+const PI = 3.14; // close enough
+const SNAP_PROBABILITY = 0.5; // perfectly balanced
